d94f9b780eb1cc5e1fb7f42342f05ff44ea09f75
R.J. Steinert <rj@rjsteinert.com>
2017-05-08 17:12:33 -0400

    Add the example from angular docs

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+import { AdService }         from './ad.service';
+import { AdItem }            from './ad-item';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <div>
+      <add-banner [ads]="ads"></add-banner>
+    </div>
+  `
+})
+export class AppComponent implements OnInit {
+  ads: AdItem[];
+
+  constructor(private adService: AdService) {}
+
+  ngOnInit() {
+    this.ads = this.adService.getAds();
+  }
+}
+
